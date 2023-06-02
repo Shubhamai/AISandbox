db949a4dc7e6bcacd97701b0d173ae5eb5eada54
Shubhamai <shubham.aiengineer@gmail.com>
2023-06-02 16:06:58 +0530

    runtime edge

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
-import { setTimeout as sleep } from "timers/promises";
+
+export const runtime = "edge";
 
 export async function POST(request: Request) {
   const res = await request.json();
-  console.log("GPT", res);
-
-  await sleep(5000);
 
   return NextResponse.json({
     data: "This is GPT4 prompt output " + res.text,
