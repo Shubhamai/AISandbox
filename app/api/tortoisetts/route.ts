e13ee51d37c52e6431a92b77bdec784a2cd07a54
Shubhamai <shubham.aiengineer@gmail.com>
2023-06-14 15:14:02 +0530

    fixing cloudflare deployment

--- app/api/tortoisetts/route.ts
+++ app/api/tortoisetts/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import Replicate from "replicate";
 
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_TOKEN as string,
-});
+// const replicate = new Replicate({
+//   auth: process.env.REPLICATE_API_TOKEN as string,
+// });
 
 export const runtime = "edge";
 
