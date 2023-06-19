755a49851cb032b2cebcd1afd470fa03b4e609fa
Shubhamai <shubham.aiengineer@gmail.com>
2023-06-19 06:10:45 +0530

    whisper runtime edge

--- app/api/(models)/whisper/route.ts
+++ app/api/(models)/whisper/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ExecuteWhisper } from "./util";
 
+export const runtime = 'edge';
+
 export async function POST(req: NextRequest) {
   const res = await ExecuteWhisper(req);
   return NextResponse.json(res);
