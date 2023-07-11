da4caa173889a3a03be7efa08bdf2bee02e5f5ce
Shubhamai <shubham.aiengineer@gmail.com>
2023-07-11 07:34:13 +0530

    nextjs 13.4.9, runtime `edge` for cloudflare

--- app/(pages)/(navbar)/api/page.tsx
+++ app/(pages)/(navbar)/api/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 const API = () => {
   return (
     <div className="flex flex-col gap-10 items-center justify-center mx-auto min-h-screen w-[400px]">
--- app/(pages)/(navbar)/docs/page.tsx
+++ app/(pages)/(navbar)/docs/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 const Docs = () => {
   return (
     <div className="flex flex-col gap-10 items-center justify-center mx-auto min-h-screen w-[400px]">
