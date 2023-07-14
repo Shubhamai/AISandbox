2ead0ad9346d73bc06f77d5d4d964a6e26087c81
Shubhamai <shubham.aiengineer@gmail.com>
2023-07-14 12:37:59 +0530

    `pnpm up`

--- app/(pages)/dashboard/apikeys/page.tsx
+++ app/(pages)/dashboard/apikeys/page.tsx
@@ -3,44 +3,6 @@ import { APIKey, columns } from "./columns";
 import { DataTable } from "./data-table";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 
-// async function getData(): Promise<APIKey[]> {
-//   // Fetch data from your API here.
-//   return [
-//     {
-//       id: "728ed52f",
-//       key: "1234567890",
-//       name: "Test",
-//       created: "2021-10-01",
-//     },
-//     {
-//       id: "3432fe",
-//       key: "1234567890",
-//       name: "abc",
-//       created: "2021-10-01",
-//     },
-//     // ...
-//   ];
-// }
-// const [data, setData] = useState<APIKey[]>([]);
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     const {
-//       data: { session },
-//     } = await supabase.auth.getSession();
-
-//     const { data, error } = await supabase
-//       .from("apikeys")
-//       .select("id,key,name,created");
-
-//     if (data) {
-//       setData(data);
-//     }
-//   };
-
-//   fetchData();
-// }, []);
-
 export default async function ApiKeys() {
   const supabase = createServerActionClient({ cookies });
 
