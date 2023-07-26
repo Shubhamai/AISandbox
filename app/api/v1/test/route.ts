096533a24ed66aa367c527187becceca4ff0c002
Shubhamai <shubham.aiengineer@gmail.com>
2023-07-26 22:31:22 +0530

    testing cloudflare kv

--- app/api/v1/test/route.ts
+++ app/api/v1/test/route.ts
@@ -1,12 +1,18 @@
 import redis from "@/app/lib/redis/client";
 import { NextRequest, NextResponse } from "next/server";
-
+import type { KVNamespace } from '@cloudflare/workers-types'
 export const runtime = "edge";
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
 
-  const value = await redis.get("test");
+  const { AISandbox } = (process.env as unknown as { AISandbox: KVNamespace });
+  
+  await AISandbox.put("Hi", "Hello");
+  
+  const value = await AISandbox.get("test");
+
+
 
   // Return 'Hello;
   return NextResponse.json({ data: value });
