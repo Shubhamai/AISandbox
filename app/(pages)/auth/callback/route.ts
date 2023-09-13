66b1fc410e75d63d53edf7e3dad3a4f72bbed5a6
Shubhamai <shubham.aiengineer@gmail.com>
2023-09-13 11:32:06 +0530

    changing callback url

--- app/(pages)/auth/callback/route.ts
+++ app/(pages)/auth/callback/route.ts
@@ -18,5 +18,6 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin + '/dashboard')
+  return NextResponse.redirect('https://aisandbox.app/dashboard')
 }
+
